0467f626ac86582f031b7bf27860d21d02236cb1
Ram Kumar <ramkr0072@gmail.com>
2023-04-12 16:05:53 +0530

    added lineclamp and singleClick feature

--- src/components/Icons/Icon.js
+++ src/components/Icons/Icon.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Icon = ({ icon }) => {
+    const [isSingleClick, setIsSingleClick] = useState(false)
+
+    const singleClick = () => {
+        setIsSingleClick(!isSingleClick)
+    }
 
 
     return (
-        <div className="w-20 text-center p-1 icons">
+        <div className={`w-14 text-center p-1 icons leading-3 ${isSingleClick ? "singleSelect":"line-clamp-1 "} text-white`} onClick={singleClick}>
             <img src={icon.icon} alt="" className='' />
-            <span className="text-white ">{icon.name}</span>
+            <span className="text-sm">{icon.name}</span>
         </div>
     )
 }
--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -3,14 +3,14 @@ import windows from '../../assets/windows.png'
 
 const Taskbar = () => {
     return (
-        <div className="w-full h-14 fixed bottom-0">
+        <div className="w-full h-12 fixed bottom-0">
             <div className="absolute inset-0 backdrop-filter backdrop-blur-3xl"></div>
             <div className="absolute inset-0 bg-white opacity-50"></div>
-            <div className="relative z-10 w-14 h-full ml-4 flex rounded-md justify-center items-center text-white space-x-1 hover:bg-slate-300">
+            <div className="relative z-10 w-14 h-full ml-1 flex rounded-md justify-center items-center text-white space-x-1 hover:bg-slate-300">
                 <img
                     src={windows}
                     alt=""
-                    className="w-8 h-8 filter drop-shadow-md bg-white  cursor-pointer"
+                    className="w-6 h-6 filter drop-shadow-md bg-white  cursor-pointer"
                 />
             </div>
         </div>
